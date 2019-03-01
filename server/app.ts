51d2e7dbe1c0360f28a36a1bec5d826e89e4106b
Chris Figgins <figginsc@gmail.com>
2019-02-28 19:47:34 -0500

    Updated to allow CORS access

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,22 @@ import customersController from "./customers/customers.controller";
 // Create the express application
 const app = express();
 
+const allowCrossDomain = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+
+    // intercept OPTIONS method
+    if ("OPTIONS" === req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+};
+
+// Allows CORS domain
+app.use(allowCrossDomain);
+
 // Assign controllers to routes
 app.use("/api/customers", customersController);
 
